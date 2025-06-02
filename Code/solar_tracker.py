14c50c41d241fe6a5d35f0eccbe2f2f6fe3578b9
JT-Mecha <jostil7990@students.ecpi.edu>
2025-06-01 20:02:53 -0500

    Update solar_tracker.py
    Added boundaries for solar tracker servos

--- Code/solar_tracker.py
+++ Code/solar_tracker.py
@@ -15,22 +15,22 @@ positionx = 1500000
 positiony = 1500000
 
 while True:
-    if TL.value() and TR.value():
-        Yaxis.duty_ns(positiony)
-        positiony += 5000
-        time.sleep(0.01)
+       if (TL.value() == 1 and TR.value() == 1 and positiony != 2500000):   #if top left and top right LDR sensors reach their preset values
+        Yaxis.duty_ns(positiony)                # move y axis servo to saved variable
+        positiony = positiony + 5000            # add 5000 nanoseconds to duty cycle and change the variable value
+        time.sleep(0.01)                        # wait 10 milliseconds between each step
 
-    if TL.value() and BL.value():
+    if (TL.value() == 1 and BL.value() == 1 and positionx != 500000):   # same sequences as before with different sensors sets
         Xaxis.duty_ns(positionx)
-        positionx -= 5000
+        positionx = positionx - 5000            # negative indicates reverse direction
         time.sleep(0.01)
 
-    if BL.value() and BR.value():
+    if (BL.value() == 1 and BR.value() == 1 and positiony != 500000):
         Yaxis.duty_ns(positiony)
-        positiony -= 5000
+        positiony = positiony - 5000
         time.sleep(0.01)
 
-    if TR.value() and BR.value():
+    if (TR.value() == 1 and BR.value() == 1 and positionx != 2500000):
         Xaxis.duty_ns(positionx)
-        positionx += 5000
+        positionx = positionx + 5000
         time.sleep(0.01)
